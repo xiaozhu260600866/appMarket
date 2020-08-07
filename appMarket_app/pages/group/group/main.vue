66593b36745f4b9b7ca2e0d812cf0d04322b51d4
xiaozhu260600866 <260600866@qq.com>
2020-08-07 18:06:33 +0800

    meimei

--- appMarket_app/pages/group/group/main.vue
+++ appMarket_app/pages/group/group/main.vue
@@ -1,214 +1,188 @@
 <template>
 	<view>
 		<page :parentData="data" :formAction="formAction"></page>
-		<div v-if="data.show">
-			<div class="show_banner bg-f" id="arrowTop">
-				<myswiper :lists="data.covers" purl="product"></myswiper>
-			</div>
-			<div class="ass-price plr15 ptb10">
-				<p class="price fs-28"><span class="fs-12">￥</span>{{ data.product.group_price }}</p>
-				
-			</div>
-			<div class="bg-f pb10">
-				<div class="proTitle plr15 ptb10">
-					<p class="fs-15">{{ data.product.name }}</p>
-				</div>
-				<div class="proCount fs-13 fc-9 plr15 pb10">
-					<p>运费：{{ data.product.yunfei ? data.product.yunfei : '免运费' }}</p>
-					<p>剩余：<span class="Arial">{{ data.product.num }}</span></p>
-				</div>
-			</div>
-			<div class="group bgf plr15 pb10">
-				<div class="group-box p20">
-					<!-- 拼团未完成时团主及他人都显示 -->
-					<div class="con" v-if="data.detail.group.leftNum && data.detail.group.canGorupBuy">
-						<p class="ass-state">还差<span class="Arial num"> {{ data.detail.group.leftNum }} </span>人成团</p>
-						<p class="ass-disc">剩余 <span class="Arial time">
-							<leftTime v-model="data.detail.group.end_date" type="0"></leftTime>
-						</span> 结束</p>
-					</div>
-
-					<!-- 自己进入时满员显示 -->
-					<div class="con" v-if="data.detail.group.leftNum == 0 && data.detail.isMy">
-						<p class="ass-state">拼团成功</p>
-						<p class="ass-disc">恭喜您，商家尽快发货</p>
-					</div>
-
-					<!-- 别人进入时满员显示 -->
-					<div class="con" v-if="data.detail.group.leftNum == 0 && !data.detail.isMy">
-						<p class="ass-state">此团已满员</p>
-					</div>
-
-					<!-- 拼团失败时显示 -->
-					<div class="con" v-if="data.detail.group.leftNum && !data.detail.group.canGorupBuy">
-						<p class="ass-state">拼团失败</p>
-						<p class="ass-disc" style="display:none">系统将自动退款</p>
-					</div>
-
-					<!-- 拼团成功时显示 -->
-					<div class="con" v-if="data.detail.group.leftNum == 0 && !data.detail.group.canGorupBuy">
-						<p class="ass-state">拼团成功</p>
-						<p class="ass-disc" style="display:none">系统将自动退款</p>
-					</div>
-
-					<div class="people">
-						<div class="people-list" v-for="(v,key) in data.detail.group.group_num">
-							<div :class="['p-item',  'main']" v-if="key == 0 && data.groupInfo[0]">
-								<image class="img" :src="data.groupInfo[0].getUser.headerPic" />
-								<p class="tab">团长</p>
-							</div>
-							<div class="p-item second" v-if="key>0">
-								<image class="img" :src="data.groupInfo[key] ? data.groupInfo[key].getUser.headerPic :'https://boss.doxinsoft.com/images/wap/group-who.png'" />
-							</div>
-						</div>
-
-					</div>
-				</div>
-			</div>
-			<div class="ass-rule mt8 bgf plr15">
-				<div class="title ptb8">
-					<p class="fs-16">拼团说明</p>
-				</div>
-				<div class="process pt5 pb10">
-					<div class="item">
-						<span class="iconfont icon-float-feedback"></span>
-						<span class="name">开团/参团</span>
-					</div>
-					<div class="item">
-						<span class="iconfont icon-float-feedback"></span>
-						<span class="name">邀友参团</span>
-					</div>
-					<div class="item">
-						<span class="iconfont icon-float-feedback"></span>
-						<span class="name">人满成团</span>
-					</div>
-				</div>
-				<div class="rule-con pb15">
-					<p class="fs-14">支付开团或参团，邀请好友参团，在有效时间内达到成团人数则拼团成功，进行发货；若超时未达到拼团人数则拼团失败，系统将自动退款。</p>
-				</div>
-			</div>
-			
-			<!-- 商品评价&商品详情 -->
-			<div class="pro_title mt10 bg-f">
-				<div class="title-group" @click="showEvaluate=false">
-					<span :class="['span','fs-16',!showEvaluate ? 'cur' :'']">商品详情</span>
-				</div>
-				<div class="title-group" @click="showEvaluate=true">
-					<span :class="['span','fs-16',showEvaluate ? 'cur' : '']">评价</span>
-				</div>
-			</div>
-			<view class="pro-content bg-f" v-if="!showEvaluate && data.product">
-				<view class="pro-con-main">
-					<uParse :content="data.product.content" />
+		<view class="pb60">
+			<view class="show_banner bg-f">
+				<myswiper id="arrowTop" :tbPadding="0" :lrPadding="0" :bdR="0" :data="[
+					{cover: '/static/pro01.jpg'},
+					{cover: '/static/pro02.jpg'}
+				]" purl="product"></myswiper>
+			</view>
+			<view class="ass-price plr15 ptb10">
+				<view class="price fs-28"><text class="fs-12">￥</text>{{ data.product.group_price }}</view>
+				<view class="r-time" v-if="data.product.group_expire_date!='2099-12-30' || data.product.group_expire_date != '2099-12-30 00:00:00'">
+					<view v-if="data.product.canGorupBuy">
+						<view class="fc-white fs-12">距离活动结束还剩</view>
+						<leftTime v-model="data.product.group_expire_date" type="1"></leftTime>
+					</view>
+					<view v-else>
+						<view class="fc-white fs-18">活动已结束</view>
+					</view>
 				</view>
 			</view>
-			<div class="evalute pb50 bd-be" v-if="showEvaluate">
-				<div class="evalute-item p10 bg-f bd-be" v-for="v in data.suggestLists">
-					<div class="u-info">
-						<div class="u-info-box">
-							<div class="u-img"><img class="img" :src="v.getUser.headimgurl"></div>
-							<div class="u-name pl10">
-								<p class="name lh20 fs-14">{{ v.getUser.nickname }}</p>
-								<p class="fs-12 fc-6">好评</p>
-							</div>
-							<div class="r-time Arial fs-12 fc-9" style="width:250px">{{ v.created_at }}</div>
-						</div>
-					</div>
-					<div class="u-con ptb15">
-						<p class="p">{{ v.suggestContent }}</p>
-						<div class="image-group mt5">
-							<div class="img-item" v-for="(item,index2) in v.getSuggestLogo">
-								<image :src="getSiteName + '/upload/images/order/'+item " mode="aspectFill"></image>
-							</div>
-						</div>
-					</div>
-					<div class="u-con ptb15 bd-te" v-if="v.suggestStatus ==2">
-						<p class="fs-color fs-14 p" style="width:250px">{{ v.updated_at }}追评</p>
-						<p class="p">{{ v.suggestContent2 }}</p>
-						<div class="image-group mt5">
-							<div class="img-item" v-for="(item,index2) in v.getSuggestLogo2">
-								<image :src="getSiteName + '/upload/images/order/'+item " mode="aspectFill"></image>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="data-con" v-if="data.suggestLists.length == 0">
-					<p class="fs-12 fc-9 p10 text-center">暂无评价</p>
-				</div>
-			</div>
-			<!-- 商品评价&商品详情结束 -->
+			<view class="bg-f mb12">
+				<view class="proTitle plr15 ptb10">
+					<view class="fs-15">{{ data.product.name }}</view>
+				</view>
+				<view class="proCount fs-13 fc-9 plr15 pb10">
+					<view>运费：{{ data.product.yunfei ? data.product.yunfei : '免运费' }}</view>
+					<view>剩余：<text class="Arial">{{ data.product.num }}</text></view>
+				</view>
+			</view>
+			<view class="pro_type bg-f mtb12">
+				<dx-list-msg name="新鲜度" content="新鲜库越高，菜品越新鲜" :nameSize="15">
+					<view slot="right">
+						<view class="fs-15 Arial main-color">{{proDetail.fresh}}</view>
+					</view>
+				</dx-list-msg>
+				<dx-list-msg name="适用人群" :nameSize="15">
+					<view slot="right">
+						<view class="fs-14 fc-3">{{proDetail.apply}}</view>
+					</view>
+				</dx-list-msg>
+			</view>
 			
-			<div id="footer">
-				<div class="left plr8">
+			<view class="ass-rule mt12 bg-f plr15">
+				<view class="title ptb8">
+					<view class="fs-16">拼团说明</view>
+				</view>
+				<view class="process pt5 pb10">
+					<view class="item">
+						<text class="iconfont icon-float-feedback"></text>
+						<text class="name">开团/参团</text>
+					</view>
+					<view class="item">
+						<text class="iconfont icon-float-feedback"></text>
+						<text class="name">邀友参团</text>
+					</view>
+					<view class="item">
+						<text class="iconfont icon-float-feedback"></text>
+						<text class="name">人满成团</text>
+					</view>
+				</view>
+				<view class="rule-con pb15">
+					<view class="fs-14">支付开团或参团，邀请好友参团，在有效时间内达到成团人数则拼团成功，进行发货；若超时未达到拼团人数则拼团失败，系统将自动退款。</view>
+				</view>
+			</view>
+			<view class="group bg-f plr15 ptb12">
+				<view class="group-box p20">
+					<!-- 拼团未完成时团主及他人都显示 -->
+					<view class="con" v-if="data.detail.group.leftNum && data.detail.group.canGorupBuy">
+						<view class="ass-state">还差<text class="Arial num"> {{ data.detail.group.leftNum }} </text>人成团</view>
+						<view class="ass-disc">剩余 <text class="Arial time">
+							<leftTime v-model="data.detail.group.end_date" type="0"></leftTime>
+						</text> 结束</view>
+					</view>
+			
+					<!-- 自己进入时满员显示 -->
+					<view class="con" v-if="data.detail.group.leftNum == 0 && data.detail.isMy">
+						<view class="ass-state">拼团成功</view>
+						<view class="ass-disc">恭喜您，商家尽快发货</view>
+					</view>
+			
+					<!-- 别人进入时满员显示 -->
+					<view class="con" v-if="data.detail.group.leftNum == 0 && !data.detail.isMy">
+						<view class="ass-state">此团已满员</view>
+					</view>
+			
+					<!-- 拼团失败时显示 -->
+					<view class="con" v-if="data.detail.group.leftNum && !data.detail.group.canGorupBuy">
+						<view class="ass-state">拼团失败</view>
+						<view class="ass-disc" style="display:none">系统将自动退款</view>
+					</view>
+			
+					<!-- 拼团成功时显示 -->
+					<view class="con" v-if="data.detail.group.leftNum == 0 && !data.detail.group.canGorupBuy">
+						<view class="ass-state">拼团成功</view>
+						<view class="ass-disc" style="display:none">系统将自动退款</view>
+					</view>
+			
+					<view class="people">
+						<view class="people-list" v-for="(v,key) in data.detail.group.group_num">
+							<view :class="['p-item',  'main']" v-if="key == 0 && data.groupInfo[0]">
+								<image class="img" :src="data.groupInfo[0].getUser.headerPic" /> 
+								<view class="tab">团长</view>
+							</view>
+							<view class="p-item second" v-if="key>0">
+								<image class="img" :src="data.groupInfo[key] ? data.groupInfo[key].getUser.headerPic :'https://boss.doxinsoft.com/images/wap/group-who.png'" />
+							</view>
+						</view>
+			
+					</view>
+				</view>
+			</view>
+			<view id="footer">
+				<view class="left plr8">
 					<button class="btn-item" hover-class="none" @click="toIndex">
-						<p class="iconfont icon-pro-shop"></p>
-						<p class="txt">首页</p>
+						<view class="iconfont icon-pro-shop"></view>
+						<view class="txt">首页</view>
 					</button>
-				</div>
-
+				</view>
+			
 				<!-- 拼团未完成时团主或已参团人员显示 -->
-				<div class="right" v-if="data.detail.isMy && data.detail.group.leftNum > 0">
-					<div class="r-nav">
-						<div class="r-item cart r-item-yellow" @click="goto('/pages/shop/order/buy/detail?order_no='+data.detail.order_no,1)">查看订单</div>
-					</div>
-					<div class="r-nav">
+				<view class="right" v-if="data.detail.isMy && data.detail.group.leftNum > 0">
+					<view class="r-nav">
+						<view class="r-item cart r-item-yellow" @click="goto('/pages/shop/order/buy/detail?order_no='+data.detail.order_no,1)">查看订单</view>
+					</view>
+					<view class="r-nav">
 						<button class="r-item buy r-item-red" open-type="share">邀请好友参团</button>
-					</div>
-				</div>
+					</view>
+				</view>
 				<!-- 拼团未完成时其他人进入显示 -->
-				<div class="right" v-if="!data.detail.isMy && data.detail.group.leftNum > 0">
-					<div class="r-nav">
-						<myform :append="true":ruleform="{}"  :vaildate="{}"  @callBack="toBuy(0,1)">
-							<div slot="content">
-								<div class="r-item cart r-item-yellow">我来开新团</div>
-							</div>
+				<view class="right" v-if="!data.detail.isMy && data.detail.group.leftNum > 0">
+					<view class="r-nav">
+						<myform :append="true" :ruleform="{}" :vaildate="{}" @callBack="toBuy(0,1)">
+							<view slot="content">
+								<view class="r-item cart r-item-yellow">我来开新团</view>
+							</view>
 						</myform>
-					</div>
-					<div class="r-nav">
-						<myform :append="true":ruleform="{}"  :vaildate="{}"  @callBack="toBuy(data.detail.group.id,1)">
-							<div slot="content">
-								<div class="r-item buy r-item-red" @click="toBuy(data.detail.group.id,1)">立即参与</div>
-							</div>
+					</view>
+					<view class="r-nav">
+						<myform :append="true" :ruleform="{}" :vaildate="{}" @callBack="toBuy(data.detail.group.id,1)">
+							<view slot="content">
+								<view class="r-item buy r-item-red" @click="toBuy(data.detail.group.id,1)">立即参与</view>
+							</view>
 						</myform>
-					</div>
-					
-				</div>
-
+					</view>
+			
+				</view>
+			
 				<!-- 拼团完成时其人进入显示 -->
-				<div class="right" v-if="data.detail.group.leftNum <= 0 && !data.detail.isMy">
-					<div class="r-nav">
-						<myform :append="true" :ruleform="{}"  :vaildate="{}"  @callBack="toBuy(0,1)">
-							<div slot="content">
-								<div class="r-item buy o-btn r-item-red" @click="toBuy(0,1)">我来开新团</div>
-							</div>
+				<view class="right" v-if="data.detail.group.leftNum <= 0 && !data.detail.isMy">
+					<view class="r-nav">
+						<myform :append="true" :ruleform="{}" :vaildate="{}" @callBack="toBuy(0,1)">
+							<view slot="content">
+								<view class="r-item buy o-btn r-item-red" @click="toBuy(0,1)">我来开新团</view>
+							</view>
 						</myform>
-					</div>
-				</div>
-
+					</view>
+				</view>
+			
 				<!-- 拼团成功时显示 -->
-				<div class="right" v-if="data.detail.group.leftNum <= 0 && data.detail.isMy">
-					<div class="r-nav">
-						<myform :append="true":ruleform="{}"  :vaildate="{}"  @callBack="goto('/pages/shop/order/buy/detail?order_no='+data.detail.order_no,1)">
-							<div slot="content">
-								<div class="r-item cart r-item-yellow">查看订单</div>
-							</div>
+				<view class="right" v-if="data.detail.group.leftNum <= 0 && data.detail.isMy">
+					<view class="r-nav">
+						<myform :append="true" :ruleform="{}" :vaildate="{}" @callBack="goto('/pages/shop/order/buy/detail?order_no='+data.detail.order_no,1)">
+							<view slot="content">
+								<view class="r-item cart r-item-yellow">查看订单</view>
+							</view>
 						</myform>
-					</div>
-					<div class="r-nav">
-						<myform :append="true":ruleform="{}"  :vaildate="{}"  @callBack="goto('/pages/shop/index/index',2)">
-							<div slot="content">
-								<div class="r-item buy r-item-red" @click="goto('/pages/shop/index/index',2)">更多拼团</div>
-							</div>
+					</view>
+					<view class="r-nav">
+						<myform :append="true" :ruleform="{}" :vaildate="{}" @callBack="goto('/pages/shop/index/index',2)">
+							<view slot="content">
+								<view class="r-item buy r-item-red" @click="goto('/pages/shop/index/index',2)">更多拼团</view>
+							</view>
 						</myform>
-					</div>
-				</div>
-
+					</view>
+				</view>
+			
 				<!-- 拼团失败显示 -->
-				<div class="right" style="display: none;">
-					<div class="r-item buy o-btn r-item-red" @click="toBuy(0,1)">重新开团</div>
-				</div>
-			</div>
-		</div>
+				<view class="right" style="display: none;">
+					<view class="r-item buy o-btn r-item-red" @click="toBuy(0,1)">重新开团</view>
+				</view>
+			</view>
+			
+		</view>
 		<info :productInfo="data.productInfo" @callback="infoCallBack" ref="productInfo"></info>
 	</view>
 </template>
@@ -217,14 +191,57 @@
 	import leftTime from "../show/layouts/left_time";
 	import info from '../show/layouts/info'
 	import uParse from '@/components/gaoyia-parse/parse.vue'
+	import dxListMsg from "doxinui/components/list-cell/list-msg"
 	export default {
+		components: {leftTime,uParse,info,dxListMsg},
 		data() {
 			return {
 				formAction: '/shop/order/group',
 				mpType: 'page', //用来分清父和子组件
-				data: this.formatData(this),
+				// data: this.formatData(this),
 				getSiteName: this.getSiteName(),
-				showEvaluate: false,
+				data:{
+					product:{
+						group_price: 6.6,
+						group_expire_date: '2020-08-01',
+						group_expire_date: '2020-08-31',
+						canGorupBuy: true,
+						name: '新鲜水果',
+						yunfei: '',
+						num: 666,
+						price: 10.00,
+					},
+					orderGroup:[{
+						getUser:{ 
+							headerPic: '/static/logo.png',
+							nickname: '同城菜场',
+						},
+						leftNum: 1,
+						end_date: '2020-08-31'
+					},{
+						getUser:{ 
+							headerPic: '/static/logo.png',
+							nickname: '同城菜场',
+						},
+						leftNum: 1,
+						end_date: '2020-08-31'
+					}],
+					detail:{
+						isMy: false,
+						group:{
+							leftNum: 2,
+							canGorupBuy: true,
+							end_date: '2020-08-08',
+
+						}
+					}
+				},
+				proDetail:{
+					fresh: 9,
+					apply: '全部',
+					content:'产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情'
+				},
+
 			}
 		},
 		onReachBottom() {
@@ -232,7 +249,7 @@
 		},
 		onPullDownRefresh() {
 			this.data.nextPage = 1;
-			this.ajax();
+			//this.ajax();
 		},
 		onShareAppMessage() {
 			let title = '[仅剩'+this.data.detail.group.leftNum+'个名额]我'+this.data.detail.amount+'元拼了'+this.data.product.name;
@@ -240,9 +257,9 @@
 			return this.shareSource(this, title,"1");
 		},
 		onLoad(options) {
-			setTimeout(()=>{
-				this.ajax();
-			},500)
+			// setTimeout(()=>{
+			// 	this.ajax();
+			// },500)
 			
 		},
 		methods: {
@@ -277,11 +294,6 @@
 					console.log(this.data);
 				});
 			}
-		},
-		components: {
-			leftTime,
-			uParse,
-			info
 		},
 	}
 </script>
--- appMarket_app/pages/group/show/main.vue
+++ appMarket_app/pages/group/show/main.vue
@@ -8,7 +8,130 @@
 					{cover: '/static/pro02.jpg'}
 				]" purl="product"></myswiper>
 			</view>
-			
+			<view class="ass-price plr15 ptb10">
+				<view class="price fs-28"><text class="fs-12">￥</text>{{ data.product.group_price }}</view>
+				<view class="r-time" v-if="data.product.group_expire_date!='2099-12-30' || data.product.group_expire_date != '2099-12-30 00:00:00'">
+					<view v-if="data.product.canGorupBuy">
+						<view class="fc-white fs-12">距离活动结束还剩</view>
+						<leftTime v-model="data.product.group_expire_date" type="1"></leftTime>
+					</view>
+					<view v-else>
+						<view class="fc-white fs-18">活动已结束</view>
+					</view>
+				</view>
+			</view>
+			<view class="bg-f mb12">
+				<view class="proTitle plr15 ptb10">
+					<view class="fs-15">{{ data.product.name }}</view>
+				</view>
+				<view class="proCount fs-13 fc-9 plr15 pb10">
+					<view>运费：{{ data.product.yunfei ? data.product.yunfei : '免运费' }}</view>
+					<view>剩余：<text class="Arial">{{ data.product.num }}</text></view>
+				</view>
+			</view>
+			<view class="pro_type bg-f mtb12">
+				<dx-list-msg name="新鲜度" content="新鲜库越高，菜品越新鲜" :nameSize="15">
+					<view slot="right">
+						<view class="fs-15 Arial main-color">{{proDetail.fresh}}</view>
+					</view>
+				</dx-list-msg>
+				<dx-list-msg name="适用人群" :nameSize="15">
+					<view slot="right">
+						<view class="fs-14 fc-3">{{proDetail.apply}}</view>
+					</view>
+				</dx-list-msg>
+			</view>
+			<view class="ass-group bg-f mb12" v-if="data.orderGroup.length">
+				<view class="lh-40 fs-14 plr15">以下小伙伴正在发起拼团，你可以直接参与</view>
+				<view class="bd-te">
+					<dx-list-msg :imgSrc="v.getUser.headerPic" :imgWidth="40" :imgHeight="40" :name="v.getUser.nickname" :nameSize="14"
+					 v-for="v in data.orderGroup" v-if="v.leftNum">
+						<view slot="con" class="fc-9 fs-12 mt5 flex-baseline lh-16">
+							<view>还差<text class="price plr2">{{ v.leftNum }}</text>人成团，剩余</view>
+							<view><leftTime v-model="v.end_date" type="0"></leftTime></view>
+							<view>结束</view>
+						</view>
+						<view slot="right">
+							<dx-button type="danger" size="mini" hollow  @click="goto('/pages/group/group/main?order_no='+v.order_no)">去拼团</dx-button>
+						</view>
+					</dx-list-msg>
+					<view class="lh-40 fs-13 fc-6 plr15">支付开团邀请多.人参团，人数不足自动退款</view>
+				</view>
+			</view>
+			<dx-tabs :tabs="tabs" v-model="Sstatus" selectedColor="#57C734" sliderBgColor="#57C734" :nameSize="17" :height="100"></dx-tabs>
+			<view class="pro-content" v-if="Sstatus == 0">
+				<view class="pro-con-main bg-f p10 fs-15 lh-22">
+					<u-parse :content="proDetail.content"/>
+				</view>
+				<view class="data-con">
+					<view class="fs-12 fc-9 p20 text-center">暂无详情</view>
+				</view>
+			</view>
+			<view class="evalute" v-if="Sstatus == 1">
+				<view class="evalute-item p10 bg-f bd-be" v-for="v in suggestLists">
+					<view class="u-info">
+						<view class="u-info-box">
+							<view class="u-img">
+								<image class="img" :src="v.headimgurl" />
+							</view>
+							<view class="u-name pl10">
+								<view class="name lh-20 fs-14">{{ v.nickname }}</view>
+								<tui-rate :value="v.quote" :disabled="true"></tui-rate>
+							</view>
+							<view class="r-time Arial fs-13 fc-9 pl10">{{ getforMatDate(v.created_at) }}</view>
+						</view>
+					</view>
+					<view class="u-con pt10 plr15">
+						<view class="p">{{ v.suggestContent }}</view>
+						<dx-images :data="v.getSuggestLogo"></dx-images>
+					</view>
+				
+				</view>
+				<view class="data-con">
+					<view class="fs-12 fc-9 p20 text-center">暂无评价</view>
+				</view>
+			</view>
+			<view id="footer">
+				<view class="left plr8">
+					<button class="btn-item share bdr0" hover-class="none" @click="goto('/pages/shop/index/index', 2)">
+						<p class="iconfont icon-pro-shop"></p>
+						<p class="txt">商城</p>
+					</button>
+					<button class="btn-item share bdr0" hover-class="none" open-type="share">
+						<p class="iconfont icon-pro-share"></p>
+						<p class="txt">分享</p>
+					</button>
+				</view>
+				<view class="right">
+					<view class="r-nav">
+						<myform title="加入购物车" :append="true" :ruleform="{}" :vaildate="{}" @callBack="toBuy(0,0)">
+							<view slot="content">
+								<view class="r-item r-item-yellow">
+									<text class="fs-12">￥</text><text class="Arial">{{ data.product.price }}</text>
+									<text class="fs-15 pl3">单买</text>
+								</view>
+							</view>
+						</myform>
+					</view>
+					<view class="r-nav">
+						<myform title="加入购物车" :append="true" :ruleform="{}" :vaildate="{}" @callBack="toBuy(0,1)">
+							<view slot="content">
+								<view class="r-item r-item-red">
+									<text class="fs-12">￥</text><text class="Arial">{{ data.product.group_price }}</text>
+									<text class="fs-15 pl3">开团</text>
+								</view>
+							</view>
+						</myform>
+					</view>
+				</view>
+			</view>
+			<info :productInfo="data.productInfo" @callback="infoCallBack" ref="productInfo" :infoArr="data.infoArr" :infoArr2="data.infoArr2"></info>
+			<view v-if="show" class="pb10" style="position: absolute;left:-1300px">
+				<!-- 海报 -->
+				<sharePic :proshareBg="proshareBg" :data="data" :productFirstCover="productFirstCover" ref="sharePic"></sharePic>
+				<shareProduct2 :data="data" v-if="data.show && data.dis" ref="shareProduct" :proposterBg="proposterBg" :fingerprint="fingerprint"
+				 :qrcodeFilePath="qrcodeFilePath" :posterFilePath="posterFilePath"></shareProduct2>
+			</view>
 		</view>
 	</view>
 </template>
@@ -20,33 +143,92 @@
 	import sharePic from "@/components/poster/sharePic.vue";
 	import leftTime from "./layouts/left_time";
 	import info from './layouts/info'
+	import dxListMsg from "doxinui/components/list-cell/list-msg"
+	import dxTabs from "doxinui/components/tabs/tabs"
+	import tuiRate from "xiaozhu/uniapp/thorui/components/rate/rate"
+	import dxImages from "doxinui/components/images/images"
 	export default {
+		components: {uParse,info,leftTime,shareProduct2,sharePic,dxListMsg,dxTabs,tuiRate,dxImages},
 		data() {
 			return {
 				formAction: '/shop/product/show',
 				mpType: 'page', //用来分清父和子组件
-				data: this.formatData(this),
-				qrcodeFilePath:'',
-				posterFilePath:'',
-				proposterBg: '',
-				tempFilePath:'',
+				// data: this.formatData(this),
 				getSiteName: this.getSiteName(),
-				num: 1,
-				showEvaluate: false,
-				keepAlive: false,
-				productFirstCover:'',
-				showType: '',
-				show:false,
-				intoView: '',
-				shareDiag: false,
-				proshareBg:'',
-				fingerprint:'',
-				prodheadBg:'',
-				rule_type: 0,
-				videoUrl:'',
-				otherData: {
-					card_user_id: ''
+				data:{
+					product:{
+						group_price: 6.6,
+						group_expire_date: '2020-08-01',
+						group_expire_date: '2020-08-31',
+						canGorupBuy: true,
+						name: '新鲜水果',
+						yunfei: '',
+						num: 666,
+						price: 10.00,
+					},
+					orderGroup:[{
+						getUser:{ 
+							headerPic: '/static/logo.png',
+							nickname: '同城菜场',
+						},
+						leftNum: 1,
+						end_date: '2020-08-31'
+					},{
+						getUser:{ 
+							headerPic: '/static/logo.png',
+							nickname: '同城菜场',
+						},
+						leftNum: 1,
+						end_date: '2020-08-31'
+					}],
 				},
+				proDetail:{
+					fresh: 9,
+					apply: '全部',
+					content:'产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情产品详情'
+				},
+				Sstatus: 0,
+				tabs: [{
+					value: 0,
+					name: "商品详情"
+				}, {
+					value: 1,
+					name: "评价"
+				}],
+				suggestLists:[{
+					headimgurl:'/static/banner01.jpg',
+					nickname:'东信科技-梅',
+					quote:4,
+					created_at: '2020-06-18 11:29:32',
+					suggestContent:'非常新鲜，送货也很快，很值得购买，方便快捷，特别适合上班一族',
+					getSuggestLogo:[
+						{img:'/static/pro01.jpg'},
+						{img:'/static/pro02.jpg'},
+						{img:'/static/pro03.jpg'},
+						{img:'/static/pro02.jpg'},
+						{img:'/static/pro03.jpg'},
+						{img:'/static/pro01.jpg'},
+						{img:'/static/pro03.jpg'},
+						{img:'/static/pro01.jpg'},
+						{img:'/static/pro02.jpg'},
+					]
+				},{
+					headimgurl:'/static/banner01.jpg',
+					nickname:'东信科技-梅',
+					quote:5,
+					created_at: '2020-06-18 11:29:32',
+					suggestContent:'非常新鲜，送货也很快，很值得购买，方便快捷，特别适合上班一族',
+					getSuggestLogo:[
+						{img:'/static/pro01.jpg'},
+					]
+				},{
+					headimgurl:'/static/banner01.jpg',
+					nickname:'东信科技-梅',
+					quote:3,
+					created_at: '2020-06-18 11:29:32',
+					suggestContent:'非常新鲜，送货也很快，很值得购买，方便快捷，特别适合上班一族',
+				}],
+				
 			}
 		},
 		onReachBottom() {
@@ -70,6 +252,11 @@
 			//this.ajax();
 		},
 		methods: {
+			getforMatDate(created_at){
+				let date2=created_at.replace(new RegExp(/-/gm) ,"/");
+				let date1 = new Date(date2);
+				return this.dateToString(date1);
+			},
 			ajax() {
 				this.getAjax(this).then(msg => {
 					if(msg.product.video_url){
@@ -213,14 +400,6 @@
 					}
 				});
 			}
-		},
-		components: {
-			uParse,
-			info,
-			leftTime,
-			shareProduct2,
-			sharePic
-
 		}
 	}
 </script>
