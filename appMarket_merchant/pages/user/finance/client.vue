c804ed3a7facb405e19eaf971af2946ef090d346
xiaozhu260600866 <260600866@qq.com>
2020-08-21 18:18:17 +0800

    xiaozhu

--- appMarket_merchant/pages/user/finance/client.vue
+++ appMarket_merchant/pages/user/finance/client.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
 		<page :parentData="data" :formAction="formAction"></page>
-		<view>
+		<view v-if="show">
 			<view class="bg-f mb12">
 				<view class="client_title">
 					<view class="name">新老顾客</view>
@@ -10,15 +10,15 @@
 				<view class="count">
 					<view class="c-item">
 						<view class="name">成交顾客数</view>
-						<view class="num">{{count.shopping_client}}</view>
+						<view class="num">{{data.clientCount}}</view>
 					</view>
 					<view class="c-item">
 						<view class="name">新客人数</view>
-						<view class="num">{{count.news_client}}</view>
+						<view class="num">{{data.newClientCount}}</view>
 					</view>
 					<view class="c-item">
 						<view class="name">老客人数</view>
-						<view class="num">{{ count.old_client }}</view>
+						<view class="num">{{ data.oldClientCount }}</view>
 					</view>
 				</view>
 			</view>
@@ -30,33 +30,34 @@
 				</view>
 				<view class="bd-be">
 					<dx-title name="本周新增" :nameSize="15">
-						<view slot="right" class="fs-16 Arial fc-3 flex1">0</view>
+						<view slot="right" class="fs-16 Arial fc-3 flex1">{{data.weekAddClient}}</view>
 					</dx-title>
 				</view>
-				<view class="ptb50 fs-16 fc-6 text-center">曲线统计图</view>
+				<echarts :option="options" style="height: 300px;" ></echarts>
 			</view>
-			<!-- <view class="bg-f mb12">
+			<view class="bg-f mb12">
 				<view class="client_title">
 					<view class="name">复购</view>
-					<weui-input v-model="ruleform.timeQuantum" name="timeQuantum" changeField="value" type="select" dataKey="timeQuantumArr"></weui-input>
+					<!-- <weui-input v-model="ruleform.timeQuantum" name="timeQuantum" changeField="value" type="select" dataKey="timeQuantumArr"></weui-input> -->
 				</view>
-				<view class="ptb50 fs-16 fc-6 text-center">扇形统计图</view>
-			</view> -->
+				<echarts :option="options2" style="height: 300px;" ></echarts>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
 	import dxTitle from "doxinui/components/title/title"
-	let echarts = require('echarts/lib/echarts')
+	import echarts from '@/components/echarts/echarts.vue'
+
 	console.log(echarts);
 	export default {
 		components:{
-			dxTitle
+			dxTitle,echarts
 		},
 		data() {
 			return {
-				formAction: '/merchant/count',
+				formAction: '/merchant/clientcount',
 				mpType: 'page', //用来分清父和子组件
 				data: this.formatData(this),
 				getSiteName: this.getSiteName(),
@@ -70,6 +71,9 @@
 					news_client: 0,
 					old_client: 0,
 				},
+				options:{},
+				options2:{},
+				show:false,
 				timeQuantumArr: [{
 						label: '本周',
 						value: 'thisweek'
@@ -95,6 +99,38 @@
 					searchType:this.ruleform.searchType
 				}).then(msg => {
 					
+						this.options = {
+							color: ['#3398DB'],
+							title: { text: '新增人数1', textStyle: { fontSize: 15 }, left: 'center', top: "20px" },
+							tooltip: {},
+							xAxis: {
+								data: this.data.data.client.name
+							},
+							yAxis: {},
+							series: [{
+								name: '人数',
+								type: 'bar',
+								data: this.data.data.client.value
+							}]
+						};
+						this.options2 = {
+							type: 'pie',
+							radius: '65%',
+							center: ['50%', '50%'],
+							selectedMode: 'single',	
+						
+							series: [{
+								name: '人数',
+								type: 'pie',
+								data: [
+									{value:100,name:'复购新客100%'},
+									{value:30,name:'复购老客0%'},
+								]
+							}]
+						};
+					
+					this.show = true;
+					console.log(this.options);
 				});
 			}
 		},
