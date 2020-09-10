4082e43741dd4cec7b13b2b262ece711279b2b20
xiaozhu260600866 <260600866@qq.com>
2020-09-10 18:09:58 +0800

    xiaozhu

---
+++ appMarket_admin/src/views/integral/order/lists.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <dx-table :data="data" :global-data="globalData" :operate-width="180" @submitBeforeCallBack="submitBeforeCallBack" />
+  </div>
+</template>
+<script type="text/javascript">
+    import globalData from './layouts/order.js'
+    export default {
+        components: {
+            'dx-table': resolve => require(['xiaozhu/elementAdmin/components/dx_table.vue'], resolve)
+
+        },
+        data() {
+            return {
+                formAction: '/admin/integral/order-lists',
+                data: this.formatData(this),
+                siteName: this.getSiteName(),
+                globalData: globalData
+            }
+        },
+        watch: {
+            '$route': 'ajax'
+        },
+        mounted() {
+            this.ajax()
+        },
+        methods: {
+
+            submitBeforeCallBack(ruleform) {
+                delete ruleform.api_token
+                console.log(ruleform)
+                this.$set(ruleform, 'role', 7)
+            },
+            ajax() {
+                this.getAjax(this, {}, msg => {
+
+                })
+            }
+        }
+    }
+</script>
