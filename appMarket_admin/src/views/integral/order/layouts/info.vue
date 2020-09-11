d81164259f78b7ffcfbba34c8aa24b1a0deb576e
xiaozhu260600866 <260600866@qq.com>
2020-09-11 17:05:04 +0800

    xiaozhu

---
+++ appMarket_admin/src/views/integral/order/layouts/info.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- 上传规格 -->
+  <el-dialog title="修改订单内容" :visible.sync="dialogVisible" size="tiny" :before-close="handleClose" />
+</template>
+<script type="text/javascript">
+export default {
+    // props: ['type', 'ruleForm', 'formAction'],
+    data() {
+        return {
+            dialogVisible: false
+        }
+    }, // 父类的数据
+    methods: {
+        handleClose: function(done) {
+            this.dialogVisible = false
+        },
+        ajax() {
+            this.dialogVisible = true
+        },
+        submitForm(formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    this.ruleForm.type = this.type
+                    this.postAjax(this.formAction, this.ruleForm, msg => {
+                        // if (msg.data.status == 2) {
+                        //     this.getSuccess('操作成功')
+                        //     this.$parent.ajax()
+                        //     this.dialogVisible = false
+                        // }
+                    })
+                } else {
+                    console.log('error submit!!')
+                    return false
+                }
+            })
+        }
+        /* 商品规格结束*/
+    }
+
+}
+
+</script>
