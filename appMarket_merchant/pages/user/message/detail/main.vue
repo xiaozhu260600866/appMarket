79bbce9a9f19551f220330b72fb61e27b4d3b036
xiaozhu260600866 <260600866@qq.com>
2020-09-16 10:53:44 +0800

    meimei

--- appMarket_merchant/pages/user/message/detail/main.vue
+++ appMarket_merchant/pages/user/message/detail/main.vue
@@ -2,25 +2,27 @@
 	<view>
 		<page :parentData="data" :formAction="formAction"></page>
 		<view class="mdetail" v-if="data.show">
-			<view class="md-item block-sec" v-for="(v,key) in data.lists.data">
-				<view class="top">
-					<view class="fs-15 fc-0 fw-bold">{{ v.title }}</view>
-					<view class="Arial fs-14 fc-6">{{ v.created_at }}</view>
+			<view class="md-item block-sec" v-for="(v,key) in data.lists.data" v-if="v.getOrder">
+				<view class="top fs-14 fc-6">
+					<view class="">订单号：<text class="Arail">{{ v.getOrder.order_no }}</text></view>
+					<view class="Arial">{{ v.created_at }}</view>
 				</view>
 				<view v-for="product in v.getOrder.products" v-if="product.getProduct">
-					<dx-products-pic :src="product.getProduct.firstCover" :imgWidth="80" :imgHeight="80" >
-						<view class="right-box">
-							<view class="row fs-15 lh-20">{{ v.content }}</view>
-								<view class="row" @click="goto('/pages/order/detail/main?order_no='+v.order_no,1)"><dx-button type="danger" size="mini" hollow>查看订单详情</dx-button></view>
+					<dx-products-pic :src="product.getProduct.firstCover" :imgWidth="40" :imgHeight="40" >
+						<view class="right-box fs-15 lh-20">
+							<view class="row">{{ product.getProduct.name }}</view>
 						</view>
 					</dx-products-pic>
 				</view>
-				<view class="text-center ptb10 fs-15 fc-6" v-else>该产品已被删除</view>
+				<view class="bbtn">
+					<view class="fs-14 fc-6">{{ v.content }}</view>
+					<dx-button type="danger" size="mini" hollow @click="goto('/pages/order/detail/main?order_no='+v.order_no,1)">查看订单详情</dx-button>
+				</view>
 			</view>
-			<hasMore :parentData="data"></hasMore>
+			<view class="text-center ptb10 fs-15 fc-6" v-else>该产品已被删除</view>
 		</view>
 	</view>
-</template>u
+</template>
 
 <script>
 	import dxProductsPic from "doxinui/components/products/pic"
@@ -30,7 +32,7 @@
 		},
 		data() {
 			return {
-				formAction: '/merchant/system-message?type=2',
+				formAction: '/user/system-message?type=2',
 				mpType: 'page', //用来分清父和子组件
 				data: this.formatData(this),
 				getSiteName: this.getSiteName(),
